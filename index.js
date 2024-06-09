7322fc2d55574ede651980342b0f21eeffec8ea1
Nancy Bechtold <nancyleebechtold@gmail.com>
2024-06-09 16:52:00 -0600

    reduce array phase 1

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+const totalBatteries = batteryBatches.reduce(function (accumulator, element) {
+    return element + accumulator;
+  }, 0);
+  console.log(totalBatteries)
+
+
+// const totalBatteries = batteryBatches.reduce(function (countObj, currentNum) {
+//   if (currentNum in countObj) {
+//     countObj[currentNum]++;
+//   } else {
+//     countObj[currentNum] = 1;
+//   }
+//   return countObj;
+// }, {});
+
+// console.log(totalBatteries); // { a: 3, b: 2, c: 1 }
+
+// const letters = ["a", "b", "c", "b", "a", "a"];
+
+// const lettersCount = letters.reduce(function (countObj, currentLetter) {
+//   if (currentLetter in countObj) {
+//     countObj[currentLetter]++;
+//   } else {
+//     countObj[currentLetter] = 1;
+//   }
+//   return countObj;
+// }, {});
+
+// console.log(lettersCount); // { a: 3, b: 2, c: 1 }
